75d06ec6191828f17df7a50efc0bed7dccf1f0ce
sushi <79702494+Kizuna01@users.noreply.github.com>
2024-10-20 18:35:21 +0800

    Update test.php

--- test.php
+++ test.php
@@ -1,8 +1,4 @@
 <?php
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    echo 123;
-} else {
-    http_response_code(405); // 返回 405 狀態碼
-    echo "Method Not Allowed";
-}
+header("Access-Control-Allow-Origin: *");
+echo 123;
 ?>
